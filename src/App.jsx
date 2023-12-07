9606f55e413e460bcbd1ad75faecde84edeffb11
hamiz <reshma9298h@gmail.com>
2023-12-07 12:54:16 +0530

    fix the bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ForgetPassword from './components/ForgetPassword';
+import ForgetPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
+import Dashboard from './components/Dashboard';
+// import ForgetPassword from './components/ForgetPassword';
+// import ResetPassword from './components/ResetPassword';
 
 const App = () => {
     return (
         <Router>
             <Routes>
+            <Route path="/" element={<Dashboard />} />
                 <Route path="/forget-password" element={<ForgetPassword />} />
                 <Route path="/reset-password/:token" element={<ResetPassword />} />
             </Routes>
